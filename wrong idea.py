228863312e3f3fd262f451dc5291c07702e9958c
Priyanshu1026 <119246035+Priyanshu1026@users.noreply.github.com>
2023-01-25 15:02:36 +0530

    Delete wrong idea.py

--- wrong idea.py
+++
@@ -1,8 +0,0 @@
-number_of_tests=int(input())
-for i in range(number_of_tests):
-    int(input())
-    string=input()
-    if string.count('Q')<=string.count('A') and string[-1]!='Q':
-        print('Yes')
-    else:
-        print('No')
